454e0a7e1e3d686d26201329cd101828b692d83e
Ivan <inzarubin80@gmail.com>
2020-09-15 22:11:32 +0300

    d

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -25,3 +25,54 @@ export const addEvent = (event) => {
   }
 
 }
+
+
+  
+export function personsFetchDataSuccess(persons) {
+  return {
+      type: "PERSONS_FETCH_DATA_SUCCESS",
+      persons
+  }
+}
+
+export function personsFetchData(url) {
+  return (dispatch) => {
+      fetch(url)
+          .then(response => {
+              if(!response.ok) {
+                  throw new Error(response.statusText);
+              }
+              return response;
+          })
+          .then(response => response.json())
+          .then(persons => dispatch(personsFetchDataSuccess(persons)))
+          .catch(()=>{});
+  }
+}
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchData: url => dispatch(personsFetchData(url))
+  };
+};
+
+
+
+const    fetchData =  url => dispatch(
+      (url)  =>  (dispatch) => {
+            fetch(url)
+                .then(response => {
+                    if(!response.ok) {
+                        throw new Error(response.statusText);
+                    }
+                    return response;
+                })
+                .then(response => response.json())
+                .then(persons => dispatch(personsFetchDataSuccess(persons)))
+                .catch(()=>{});
+        }
+      
+    );
+
+
