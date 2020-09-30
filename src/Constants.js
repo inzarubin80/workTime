ea5aba3272b30bfdc3b99df3c7f8c20be23ef940
Ivan <inzarubin80@gmail.com>
2020-09-30 23:09:14 +0300

    add

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,3 @@
 //export const API_URL = 'http://server-web-01.co.ecpp.ru/CustomerService/hs/items'
-
-
-export const API_URL = 'http://server-web-01.co.ecpp.ru/CustomerService/hs/items'
-
-//export const API_URL = 'http://81.5.72.78:12345/CustomerService/hs/items'
+export const API_URL = 'http://81.5.72.78:12345/CustomerService/hs/items'
 
