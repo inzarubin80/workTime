6aa164c0ad1e10ae45226edd5a9d336679e3bb51
Ivan <inzarubin80@gmail.com>
2020-10-04 21:17:05 +0300

    ru config

--- src/sreens/CalendarScreen.js
+++ src/sreens/CalendarScreen.js
@@ -11,7 +11,8 @@ import {
 
 import {
   ExpandableCalendar,
-  CalendarProvider
+  CalendarProvider,
+  LocaleConfig
 } from 'react-native-calendars';
 
 
@@ -51,6 +52,51 @@ const CalendarScreen = ({ navigation, setCurrentMonth, setCurrentDate, getEvents
     selectedColor: 'blue',
     //selectedTextColor: 'blue',
   }
+
+
+  LocaleConfig.locales['ru'] = {
+    monthNames: [
+      'Январь',
+      'Февраль',
+      'Март',
+      'Апрель',
+      'Май',
+      'Июнь',
+      'Июль',
+      'Август',
+      'Сентябрь',
+      'Октябрь',
+      'Ноябрь',
+      'Декабрь',
+    ],
+    monthNamesShort: [
+      'Янв',
+      'Фев',
+      'Мар',
+      'Апр',
+      'Май',
+      'Июн',
+      'Июл',
+      'Авг',
+      'Сен',
+      'Окт',
+      'Ноя',
+      'Дек',
+    ],
+    dayNames: [
+      'воскресенье',
+      'понедельник',
+      'вторник',
+      'среда',
+      'четверг',
+      'пятница',
+      'суббота',
+    ],
+    dayNamesShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    today: 'Сегодня',
+  };
+  
+  LocaleConfig.defaultLocale = 'ru';
 
   let DataTime = {};
   events.forEach((item, index, array) => {
